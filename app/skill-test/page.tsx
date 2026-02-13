fd05e7e775e628157551e1d7bae2e701e528179a
Rufiya Tasneem <mohammadroofiya786@gmail.com>
2026-02-13 18:52:16 +0530

    Fix dynamic export error

--- app/skill-test/page.tsx
+++ app/skill-test/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-export const dynamic = "force-dynamic"
 import React, { useState, useEffect } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
